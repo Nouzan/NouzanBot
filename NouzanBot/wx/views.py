f1280e6d134b8b29d4529fcba606bfb60c248609
Nouzan.Ubu <clobraindie@outlook.com>
2017-10-21 14:10:10 +0800

    fix bug

--- NouzanBot/wx/views.py
+++ NouzanBot/wx/views.py
@@ -11,13 +11,14 @@ def handle(request):
     timestamp = data.get('timestamp')
     nonce = data.get('nonce')
     echostr = data.get('echostr')
-    token = "nouzan414"
+    token = 'nouzan414'
 
     info = [token, timestamp, nonce]
     info.sort()
-    hashcode = hashlib.sha1(info[0]+info[1]+info[2]).hexdigest()
+    s = bytes(info[0] + info[1] + info[2], encoding='utf8')
+    hashcode = hashlib.sha1(s).hexdigest()
     print("handle/GET func: hashcode, signature: ", hashcode, signature)
     if hashcode == signature:
-        return HttpResponse(chostr)
+        return HttpResponse(echostr)
     else:
         return HttpResponse("")
