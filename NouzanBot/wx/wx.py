ce4e599fd903d346c20d182ed1b92f7e1a2f1db7
Nouzan.Ubu <clobraindie@outlook.com>
2017-10-22 00:24:10 +0800

    fix bug

--- NouzanBot/wx/wx.py
+++ NouzanBot/wx/wx.py
@@ -72,5 +72,5 @@ def reply(msg):
 
 
 def showMsg(msg_dict):
-    create_time = timezone.datetime.fromstamp(int(msg_dict['createTime']))
+    create_time = timezone.datetime.fromtimestamp(int(msg_dict['createTime']))
     print('*' + str(create_time) + '*用户(' + msg_dict['FromUserName'] + '):', msg_dict['Content'])
