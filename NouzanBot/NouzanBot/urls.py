4064eb9abd9496e04bdd1e3565a2f63d0c96a129
Nouzan.Ubu <clobraindie@outlook.com>
2017-10-21 13:47:14 +0800

    change the url for wx to 'wx'

--- NouzanBot/NouzanBot/urls.py
+++ NouzanBot/NouzanBot/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^data/', include('data.urls')),
-    url(r'^wx/', include('wx.urls')),
+    url(r'^wx', include('wx.urls')),
 ]
