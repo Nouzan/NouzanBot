9ce8837ddf1aa9a9570d12b14c73fdc4e52c415a
Nouzan.Ubu <clobraindie@outlook.com>
2017-10-22 22:10:46 +0800

    为WxCollectTask添加admin管理

--- NouzanBot/wx/admin.py
+++ NouzanBot/wx/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import WxUser, WxTextMsg
+from .models import WxUser, WxTextMsg, WxCollectTask
 from .bot import WxFlow
 
 admin.site.register(WxUser)
 admin.site.register(WxTextMsg)
 admin.site.register(WxFlow)
+admin.site.register(WxCollectTask)
