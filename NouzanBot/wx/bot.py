82e8f03f40b72b162693d38e5995f1d20ca57539
Nouzan.Ubu <clobraindie@outlook.com>
2017-10-22 19:42:07 +0800

    中文化命令、add-flow可以处理错误

--- NouzanBot/wx/bot.py
+++ NouzanBot/wx/bot.py
@@ -6,6 +6,9 @@ from django.utils import timezone
 class WxFlow(WxObject):
     infoStr = models.CharField(max_length=512)
 
+    def __str__(self):
+        return self.name + ': ' + self.infoStr
+
     def getNextFlow_or_Reply(self):
         if self.is_valid:
             infos = str(self.infoStr).strip().split(' ')
@@ -13,7 +16,7 @@ class WxFlow(WxObject):
             if self.name == 'flow':
                 if infos != ['']:
                     info = infos.pop(0)
-                    if info == 'add':
+                    if info == 'add' or info == '添加':
                         nextFlow = WxFlow.objects.create(
                             textMsg=self.textMsg,
                             name='add-flow',
@@ -24,7 +27,7 @@ class WxFlow(WxObject):
                         self.is_valid = False
                         self.save()
                         return nextFlow.getNextFlow_or_Reply()
-                    elif info == 'repeat':
+                    elif info == 'repeat' or info == '重复':
                         self.is_valid = False
                         self.save()
                         return reply(self.textMsg, ' '.join(infos))
@@ -39,18 +42,23 @@ class WxFlow(WxObject):
             elif self.name == 'add-flow':
                 if infos != ['']:
                     info = infos.pop(0)
-                    if info == 'test':
+                    if info == '测试':
                         self.is_valid = False
                         self.save()
                         return reply(self.textMsg, 'add-flow test')
-                    else:
+                    elif info == '.' or info == '。':
                         self.is_valid = False
                         self.save()
-                        return reply(self.textMsg, 'add-flow error')
+                        return None
+                    else:
+                        return reply(
+                            self.textMsg,
+                            "我并不知道你想添加些什么，请重新说（输入句号表示取消添加）："
+                        )
                 else:
                     return reply(
                         self.textMsg,
-                        "Please tell me what you want to add:"
+                        "请告诉我你想要添加的东西（输入句号表示取消添加）："
                     )
             else:
                 self.is_valid = False
@@ -64,7 +72,6 @@ def run(msg):
     if msg is not None:
         validFlowSet = WxFlow.objects.filter(is_valid=True, textMsg__fromUser=msg.fromUser)
         if validFlowSet.count() > 0:
-            print(validFlowSet[0])
             flow = validFlowSet[0]
             flow.infoStr = msg.content
             flow.save()
